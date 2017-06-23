cd93b91cea12018eb3d58f072eb7453c9f3a5d0b
kmng1 <kmng@alumni.nus.edu.sg>
2017-06-23 16:36:43 +0800

    with password, age and phone validation

--- public/index.js
+++ public/index.js
@@ -17,8 +17,9 @@
         regCon.contact = ""; 
         regCon.message = "";
          
-        regCon.submit = function(){
-            $http.get("/registration", {
+        regCon.submit = function(vf){
+
+            var p = $http.get("/registration", {
                 params: {
                     email: regCon.email,
                     password: regCon.password,
@@ -29,11 +30,54 @@
                     country: regCon.country,
                     contact: regCon.contact, 
                 }
-            }).then(function(result) {
-                regCon.message = "Registration Completed";
-            }).catch(function() {
-                regCon.message = "Failed";
             })
+
+            //var currentTime = new Date();
+            //console.log(currentTime.getFullYear());
+            //console.log(regCon.dob.getDate());
+            //console.log(regCon.dob.getMonth());
+            //console.log(regCon.dob.getFullYear());
+
+            if(vf){
+                
+                //Check password with regex
+                var a = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,}/.test(regCon.password);
+                //console.log(a);
+
+                //Check age
+                var currentTime = new Date();
+                var curYear = currentTime.getFullYear();
+                var curMonth = currentTime.getMonth() + 1;
+                var curDay = currentTime.getDate();
+                console.log(curYear + " " + curMonth + " " + curDay);
+                var day = regCon.dob.getDate();
+                var month = regCon.dob.getMonth() + 1;
+                var year = regCon.dob.getFullYear();
+                console.log(year + " " + month + " " + day);
+                age = curYear - year;
+                if(curMonth < month){
+                    age--;
+                }
+                if((month == curMonth) && (curDay < day)){
+                    age--;
+                }
+                
+                //Check Phone number
+                var ph = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im.test(regCon.contact);
+
+                //return message and do prcess accordingly
+                if(a == true && age >= 18 && ph == true){
+                    console.log("hi");
+                    p.then(function(result) {
+                        regCon.message = "Registration Completed";
+                    })
+                }else{
+                    p.then(function() {
+                        console.log("hi2");
+                        regCon.message = "Registration Failed";
+                    })
+                }
+            }
         }
     };
 
